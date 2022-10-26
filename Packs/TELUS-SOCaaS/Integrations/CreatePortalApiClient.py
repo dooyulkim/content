9d9b3fb138f5e829d7c2db297445532cada2eae1
dooyulkim <doo_yul.kim@telus.com>
2022-10-26 14:08:27 +0000

    finalize createPortalAPI

--- Packs/TELUS-SOCaaS/Integrations/CreatePortalApiClient.py
+++ Packs/TELUS-SOCaaS/Integrations/CreatePortalApiClient.py
@@ -1,8 +1,11 @@
 import json
 import os
 
-with open('apiclient.json', 'r+') as f:
+title = input("Enter the title of the integration. EX)SOC as a service from TELUS:")
+with open('apiClient.json', 'r+') as f:
     data = json.load(f)
+    if title:
+        data["info"]["title"] = title
     for path in data.get('paths').items():
         for methodDef in path[1].items():
             parameters = []
@@ -31,16 +34,20 @@ with open('apiclient.json', 'r+') as f:
                         )]
                 if(methodDefElm[0] == "responses"):
                     methodDefElm[1]["200"].pop("headers", None)
-                    
+
             if requestBody:
                 methodDef[1]["parameters"] = parameters
                 methodDef[1].pop("requestBody", None)
 
-new_file_name = 'new-apiclient.json'
-
+new_file_name = 'newApiClient.json'
 with open(new_file_name, 'w', encoding='utf-8') as f:
     f.write(json.dumps(data, indent=2))
 
-
-os.system('demisto-sdk openapi-codegen -i new-apiclient.json -n MSSPortal -o MSSPortal'
-          + ' -u "id" -pr mssportal -r "MSSPortal" -afv')
+name = input("Enter the name of integration: ")
+isdir = os.path.isdir(f"./{name}")
+if(isdir):
+    raise Exception("Sorry, An integration with the same name exists.")
+else:
+    command_prefix = input("Enter the command prefix. Ex) mssportal: ")
+    command = f'demisto-sdk openapi-codegen -i {new_file_name} -n {name} -o {name} -u "id" -pr {command_prefix} -r "{name}" -afv'
+    os.system(command)
