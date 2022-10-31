498fdaea10b2b64e9865c322d7da0d209bd79fa9
dooyulkim <doo_yul.kim@telus.com>
2022-10-31 20:22:36 +0000

    fixed mirroring script

--- Packs/TELUS-SOCaaS/Scripts/SOCaaSHandleXsoarPortalMirroring/SOCaaSHandleXsoarPortalMirroring.py
+++ Packs/TELUS-SOCaaS/Scripts/SOCaaSHandleXsoarPortalMirroring/SOCaaSHandleXsoarPortalMirroring.py
@@ -4,18 +4,21 @@ from CommonServerPython import *  # noqa: F401
 
 def main():
     try:
-        args = demisto.args()
-        xdr_status = args.get('new')
-        print(xdr_status)
-        id = demisto.context()["MSSPortal"]["Case"]["id"]
-        case = demisto.executeCommand("mssportal-get-case", {'id': id})
-        if(case.nbTelusPendingTasks == 0 and case.nbCustomerClosedTasks == case.nbCustomerTasks):
-            demisto.executeCommand("mssportal-resolve-case", {'id': id})
+        id = demisto.context()["MSSPortal"]["CaseInfo"]["id"]
+        findCaseResult = demisto.executeCommand("mssportal-find-case", {'id_': id})
+        if is_error(findCaseResult):
+            return_error(findCaseResult)
         else:
-            raise DemistoException('The incident cannot be resolved until all the tasks are closed on mssportal.')
+            case = findCaseResult[0]['Contents']
+            if((not case['nbTelusPendingTasks'] or case['nbTelusPendingTasks'] == 0) and case['nbCustomerClosedTasks'] == case['nbCustomerTasks']):
+                demisto.executeCommand("mssportal-resolve-case", {'id_': id, 'caseresolution_resolutionnotes': 'Closure via mirroring'})
+                print(f'Case({case["id"]}) resolved')
+            else:
+                raise DemistoException('The incident cannot be resolved until all the tasks are closed on mssportal.')
+
     except Exception as ex:
         demisto.error(traceback.format_exc())  # print the traceback
-       # return_error(f'Failed to execute HandleXsoarPortalMirroring. Error: {res}')
+        return_error(f'Failed to execute HandleXsoarPortalMirroring. Error: {str(ex)}')
 
 
 if __name__ in ('__main__', '__builtin__', 'builtins'):
